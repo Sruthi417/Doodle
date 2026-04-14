1e33a8d18217ad0bf2d8c8d2867c409b4f2b4d4e
Sruthi417 <sruthyy345@gmail.com>
2026-04-14 10:55:14 +0530

    fix

--- backend/modules/notes/note.route.js
+++ backend/modules/notes/note.route.js
@@ -19,16 +19,18 @@ const noterouter = express.Router();
 // Notes
 noterouter.post("/", verifyToken, createNote);
 noterouter.get("/", verifyToken, getNotes);
+
+// Todos — must be above /:id so Express doesn't treat "todos" as an id
+noterouter.get("/todos", verifyToken, getAllTodos);
+
 noterouter.get("/:id", verifyToken, getNoteById);
 noterouter.put("/:id", verifyToken, updateNote);
 noterouter.delete("/:id", verifyToken, deleteNote);
 noterouter.patch("/:id/favourite", verifyToken, toggleFavourite);
 
-// Todos
 noterouter.post("/:id/todo", verifyToken, addTodo);
 noterouter.patch("/:noteId/todo/:todoId", verifyToken, toggleTodo);
 noterouter.put("/:noteId/todo/:todoId", verifyToken, editTodo);
 noterouter.delete("/:noteId/todo/:todoId", verifyToken, deleteTodo);
-noterouter.get("/todos", verifyToken, getAllTodos);
 
 export default noterouter;
