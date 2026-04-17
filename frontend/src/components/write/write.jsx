6de41118b6fdd111a9e29f33e1508cc1c827a2e6
Sruthi417 <sruthyy345@gmail.com>
2026-04-17 16:07:37 +0530

    structure_write

--- frontend/src/components/write/write.jsx
+++ frontend/src/components/write/write.jsx
@@ -1,22 +1,116 @@
 import "./write.scss";
+import { useState } from "react";
+import ReactQuill, { Quill } from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
+
+const Font = Quill.import("formats/font");
+
+Font.whitelist = [
+  "arial",
+  "roboto",
+  "poppins",
+  "inter",
+  "lato",
+  "montserrat",
+  "times-new-roman",
+  "georgia",
+  "monospace",
+];
+
+Quill.register(Font, true);
+
+const Size = Quill.import("attributors/style/size");
+
+Size.whitelist = [
+  "10px",
+  "12px",
+  "14px",
+  "16px",
+  "18px",
+  "20px",
+  "24px",
+  "28px",
+  "32px",
+  "36px",
+  "42px",
+  "48px",
+];
+
+Quill.register(Size, true);
+
+const modules = {
+  toolbar: [
+    [{ font: Font.whitelist }],
+    [{ size: Size.whitelist }],
+
+    ["bold", "italic", "underline", "strike"],
+
+    [{ color: [] }, { background: [] }],
+
+    [{ script: "sub" }, { script: "super" }],
+
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ list: "ordered" }, { list: "bullet" }],
+
+    [{ align: [] }],
+
+    ["link", "image"],
+
+    ["clean"],
+  ],
+};
+const formats = [
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "color",
+  "background",
+  "script",
+  "header",
+  "list",
+  "bullet",
+  "align",
+  "link",
+  "image",
+];
 
 const Write = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
   return (
     <div className="writing">
       <div className="noting">
         <div className="head">
-          <div
+          <textarea
             className="titles"
-            placeholder="Title..."
+            placeholder="Title"
             value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          ></div>
+            rows={1}
+            onChange={(e) => {
+              setTitle(e.target.value);
+              e.target.style.height = "auto";
+              e.target.style.height = e.target.scrollHeight + "px";
+            }}
+          />
           <button className="to-add">Add ToDo</button>
         </div>
-        <div className="contents">Write your notes here...</div>
+        <div className="contents">
+          <ReactQuill
+            value={content}
+            onChange={setContent}
+            modules={modules} 
+            formats={formats}
+            placeholder="Write your notes here..."
+          />
+        </div>
       </div>
     </div>
   );
 };
 
-export default Write
+export default Write;
