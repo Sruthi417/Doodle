c0b6ddbac53f90f1a99bb628b398d25dd2fd14e1
Sruthi417 <sruthyy345@gmail.com>
2026-04-21 00:27:35 +0530

    deploy_fix

--- backend/modules/auth/auth.route.js
+++ backend/modules/auth/auth.route.js
@@ -32,12 +32,13 @@ authrouter.get(
       secure: isProd,
       sameSite: isProd ? "None" : "Lax",
       path: "/",
+      domain: ".onrender.com",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     console.log("Redirecting to:", `${process.env.CLIENT_URL}/home`);
     res.redirect(`${process.env.CLIENT_URL}/home`);
-  }
+  },
 );
 // LOGOUT
 authrouter.get("/logout", (req, res) => {
